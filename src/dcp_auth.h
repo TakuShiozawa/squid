2b28d6aec32e3b23309936f94aa2f8fdffda8c35
shio310 <shiozawa.taku@gmail.com>
2016-12-27 09:59:01 +0900

    Add dpc_auth.cc .h to src

---
+++ src/dcp_auth.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 1996-2016 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#ifndef _SQUID_DCP_AUTH_H
+#define _SQUID_DCP_AUTH_H
+
+#include "hash.h"
+#include "SquidString.h"
+
+void dcp_auth_calulate(String &);
+
+#endif /* _SQUID_DCP_AUTH_H */
